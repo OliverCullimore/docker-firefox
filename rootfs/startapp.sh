1b5ba50f802609028222c1c7111136706b9c2d21
Oliver Cullimore <hello@olivercullimore.co.uk>
2021-09-06 08:44:37 +0100

    Add environment variable control for kiosk mode

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,4 +6,11 @@ set -u # Treat unset variables as an error.
 export HOME=/config
 mkdir -p /config/profile
 firefox --version
-exec /usr/bin/firefox_wrapper --kiosk --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+if [ "$KIOSK_MODE" == "1" ]
+then
+  echo "Running with Kiosk Mode enabled\n"
+  exec /usr/bin/firefox_wrapper --kiosk --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+else
+  echo "Running with Kiosk Mode disabled\n"
+  exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+fi
